01ec3a1d57889443224ffff23e023f7c01f951db
Angel Gonzalez <agonzalez@faroedtech.com>
2025-05-08 15:31:43 +0200

    Fin_proyecto

---
+++ src/app/products/components/product-carousel/product-carousel.component.ts
@@ -0,0 +1,96 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  OnChanges,
+  SimpleChanges,
+  viewChild,
+} from '@angular/core';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+
+@Component({
+  selector: 'product-carousel',
+  imports: [ProductImagePipe],
+  templateUrl: './product-carousel.component.html',
+  styles: `
+    .swiper {
+      width: 100%;
+      height: 500px;
+    }
+
+
+  `,
+})
+export class ProductCarouselComponent implements AfterViewInit, OnChanges {
+  images = input.required<string[]>();
+  swiperDiv = viewChild.required<ElementRef>('swiperDiv');
+
+  swiper: Swiper|undefined = undefined;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+    if(changes["images"].firstChange){
+      return;
+    }
+
+    if(!this.swiper) return;
+
+    this.swiper.destroy(true,true)
+
+    const paginationEl = this.swiperDiv().nativeElement?.querySelector(".swiper-pagination");
+
+    paginationEl.innerHTML = "";
+
+    //Ponemos una espera para que el cambio de DOM se aplique correctamente
+    setTimeout(() => {
+      this.swiperInit()
+    }, 100)
+
+
+
+  }
+
+
+  ngAfterViewInit(): void {
+    this.swiperInit();
+  }
+
+
+  swiperInit() {
+
+    const element = this.swiperDiv().nativeElement;
+    if (!element) return;
+
+    this.swiper = new Swiper(element, {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+      modules: [Navigation, Pagination],
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
+}
